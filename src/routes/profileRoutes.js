45b9e8c595a48bdec45fb12abfdb07699000c178
Ampuh <ampuh.rakan@gmail.com>
2022-05-11 22:44:23 +0700

    finalsss

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const { verifyTokenAccess } = require("../lib/jwtVerify");
 const Router = express.Router();
 const { profileControllers } = require("./../controllers");
-const { getProfile, editProfile } = profileControllers;
+const {
+  getProfile,
+  editProfile,
+  addProfilePhoto,
+} = require("../controllers/profileControllers");
 const upload = require("../lib/upload");
 
 const uploader = upload("/profile", "PROFILE").fields([
@@ -11,6 +15,7 @@ const uploader = upload("/profile", "PROFILE").fields([
 
 Router.get("/:username", getProfile);
 Router.patch("/editProfile", verifyTokenAccess, editProfile);
+Router.patch("/addPhotos", verifyTokenAccess, uploader, addProfilePhoto);
 // Router.patch("/editProfile/:id", uploader, editProfile);
 
 module.exports = Router;
